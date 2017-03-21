b60ee16b893fb4b9a29b0c6164307a8225b0d6cc
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2017-03-21 16:27:23 -0300

    Sequential working!

--- src/clustering/kmeans_sequential.c
+++ src/clustering/kmeans_sequential.c
@@ -2,6 +2,7 @@
 #include <kmeans/clustering/kmeans_sequential.h>
 #include <kmeans/clustering/cluster.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * Execute the KMeans Algorithm using the OPENMP library.
@@ -15,17 +16,19 @@ void KMeans_Sequential_Execute(struct KMeans *kmeans)
 	unsigned long i = 0;
 	unsigned long numberPoints = kmeans->numberPoints;
 	unsigned int numberClusters = kmeans->k;
+	struct KM_Point **points = kmeans->points;
 	do {
 		previous = KMeans_Clone(kmeans);
 		KMeans_ResetPoints(kmeans);
 		for (i = 0; i < numberPoints; ++i) {
-			struct KM_Point *point = kmeans->points[i];
+			struct KM_Point *point = points[i];
 			struct KM_Cluster *closestCluster = KMeans_FindClosestCluster(kmeans, point);
 			KM_Cluster_AddPoint(closestCluster, point);
 		}
 		for (i = 0; i < numberClusters; ++i) {
 			struct KM_Cluster *cluster = KM_List_Get(kmeans->clusters, i);
-			KM_Cluster_UpdateCentroid(cluster);
+			if (cluster->points->size > 0)
+				KM_Cluster_UpdateCentroid(cluster);
 		}
 	} while (!KMeans_ClustersHaveConverged(kmeans, previous));
 }
